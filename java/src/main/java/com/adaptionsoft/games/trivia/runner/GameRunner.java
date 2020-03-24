4a7cd246083455ac46d8a9eb4994481ad78575a4
Ewan <ewan.s@cucumberwifi.io>
2020-03-24 10:50:38 +0000

    trivia golden master test

--- java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -1,36 +1,69 @@
 
 package com.adaptionsoft.games.trivia.runner;
-import java.util.Random;
 
 import com.adaptionsoft.games.uglytrivia.Game;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Random;
+import java.util.Scanner;
 
 
 public class GameRunner {
 
-	private static boolean notAWinner;
+    private static boolean notAWinner;
 
 	public static void main(String[] args) {
+
+        Game aGame = new Game();
+
+        aGame.add("Chet");
+        aGame.add("Pat");
+        aGame.add("Sue");
+
+        Random rand = new Random();
+
+        do {
+
+			aGame.roll(rand.nextInt(5) + 1);
+
+			if (rand.nextInt(9) == 7) {
+                notAWinner = aGame.wrongAnswer();
+            } else {
+                notAWinner = aGame.wasCorrectlyAnswered();
+            }
+
+        } while (notAWinner);
+
+    }
+
+	public static void mainTest() throws FileNotFoundException {
 		Game aGame = new Game();
-		
+		setWriteOutput("sample.txt");
+
 		aGame.add("Chet");
 		aGame.add("Pat");
 		aGame.add("Sue");
-		
-		Random rand = new Random();
-	
-		do {
-			
-			aGame.roll(rand.nextInt(5) + 1);
-			
-			if (rand.nextInt(9) == 7) {
-				notAWinner = aGame.wrongAnswer();
+
+		int[] rands = {1, 1, 4, 5, 1, 3, 2, 5, 1, 1, 4, 6, 3, 5, 2, 0, 3, 0, 5, 7, 4, 0, 1, 1, 3, 3, 4, 0, 3, 1, 5, 8, 4, 3};
+
+		for (int i = 0; i < rands.length; i++) {
+			if (i % 2 == 0) {
+				aGame.roll(rands[i]);
 			} else {
-				notAWinner = aGame.wasCorrectlyAnswered();
+				if (rands[i] == 7) {
+					notAWinner = aGame.wrongAnswer();
+				} else {
+					notAWinner = aGame.wasCorrectlyAnswered();
+				}
 			}
-			
-			
-			
-		} while (notAWinner);
-		
+		}
+	}
+
+	private static void setWriteOutput(String path) throws FileNotFoundException {
+		File file = new File(path);
+		PrintStream stream = new PrintStream(file);
+		System.setOut(stream);
 	}
 }
--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -2,12 +2,33 @@ package com.adaptionsoft.games.trivia;
 
 import static org.junit.Assert.*;
 
+import com.adaptionsoft.games.trivia.runner.GameRunner;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class SomeTest {
 
 	@Test
-	public void true_is_true() throws Exception {
-		assertTrue(false);
+	public void golden_master_test() throws Exception {
+		String golden = getStringBuilder("goldenMaster.txt").toString();
+		GameRunner.mainTest();
+		String sample = getStringBuilder("sample.txt").toString();
+
+		assertEquals(golden, sample);
+	}
+
+	private static StringBuilder getStringBuilder(String path) throws FileNotFoundException {
+		StringBuilder file = new StringBuilder();
+		File goldenMaster = new File(path);
+		Scanner myReader = new Scanner(goldenMaster);
+		while (myReader.hasNextLine()) {
+			file.append(myReader.nextLine());
+		}
+		System.out.println(file.toString());
+		myReader.close();
+		return file;
 	}
 }
